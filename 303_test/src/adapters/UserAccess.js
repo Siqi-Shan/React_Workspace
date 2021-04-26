1f4bcb7466b5f6e6c3a83ef4416e98f3e3dbbced
Siqi Shan <kshan@usc.edu>
2021-04-26 00:06:08 -0700

    Update package.json and AJAX

--- 303_test/src/adapters/UserAccess.js
+++ 303_test/src/adapters/UserAccess.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://303.itpwebdev.com/~kshan/fp_test",
+    baseURL: "http://303.itpwebdev.com/~kshan/assignment12",
 });
